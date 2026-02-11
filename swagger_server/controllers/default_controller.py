0414259b5ea29f6334557b1f0bf2d16953e483c6
Oriol <oriol.fernandez.briones@student.uva.com>
2026-02-11 11:23:37 +0100

    Implement student add and remove methods

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -2,6 +2,7 @@ import connexion
 import six
 
 from swagger_server.models.student import Student  # noqa: E501
+from swagger_server.service.student_service import *
 from swagger_server import util
 
 
@@ -13,11 +14,12 @@ def add_student(body=None):  # noqa: E501
     :param body: Student item to add
     :type body: dict | bytes
 
-    :rtype: str
+    :rtype: float
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        return add(body)
+    return 500, 'error'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +32,9 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: object
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        return delete(student_id)
+    return 500, 'error'
 
 
 def get_student_by_id(student_id):  # noqa: E501
